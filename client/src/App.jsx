77c84be302112cf5f74dd564af0f5809f50219be
Vatsal <vatsalbalasra24@gmail.com>
2024-03-02 10:49:03 +0530

    routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Login from "./Pages/Authentication/Login.jsx";
+import Home from "./Pages/Home/Home.jsx";
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
       <>
-        hello
+    <BrowserRouter>
+        <Routes>
+            <Route path={'/register'} element={<Login/>}/>
+            <Route path={'/home'} element={<Home/>}/>
+        </Routes>
+    </BrowserRouter>
       </>
     )
 }
---
+++ client/src/Pages/Authentication/Login.jsx
@@ -0,0 +1,12 @@
+import {Typography} from "@mui/material";
+
+
+const Login = () => {
+    return (
+        <div>
+            <Typography variant="h1" component="div" gutterBottom>Log in</Typography>
+        </div>
+    );
+}
+
+export default Login;
